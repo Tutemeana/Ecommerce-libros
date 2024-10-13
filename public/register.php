5bb05d21e26a6404bc09b7406d62ab5c9e55644d
Tutemeana <matute24b@gmail.com>
2024-10-13 19:20:30 -0300

    Arranco de 0

--- public/register.php
+++ public/register.php
@@ -4,29 +4,6 @@ use App\Entity\User;
 
 require_once "../bootstrap.php";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'];
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); 
-    $id_cargo = (int)$_POST['id_cargo'];
 
-    $user = new User();
-    $user->setName($name)
-         ->setUsername($username)
-         ->setPassword($password)
-         ->setIdCargo($id_cargo);
-
-    $entityManager->persist($user);
-    $entityManager->flush();
-
-    echo "Usuario registrado con éxito!";
-}
 ?>
 
-<form method="POST">
-    Nombre: <input type="text" name="name" required>
-    Usuario: <input type="text" name="username" required>
-    Contraseña: <input type="password" name="password" required>
-    ID Cargo: <input type="number" name="id_cargo" required>
-    <button type="submit">Registrar</button>
-</form>
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,77 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ORM\Table(name="users")
- */
-#[ORM\Entity(repositoryClass: "App\Repository\UserRepository")]
 class User
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: "integer")]
-    private ?int $id = null;
 
-    #[ORM\Column(type: "string", length: 255)]
-    private string $name;
-
-    #[ORM\Column(type: "string", length: 255, unique: true)]
-    private string $username;
-
-    #[ORM\Column(type: "string")]
-    private string $password;
-
-    #[ORM\Column(type: "integer")]
-    private int $id_cargo;
-
-    // Getters y Setters
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    public function getUsername(): string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
-        return $this;
-    }
-
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-        return $this;
-    }
-
-    public function getIdCargo(): int
-    {
-        return $this->id_cargo;
-    }
-
-    public function setIdCargo(int $id_cargo): self
-    {
-        $this->id_cargo = $id_cargo;
-        return $this;
-    }
 }
