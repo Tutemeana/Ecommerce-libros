724bf55f3ed1cc69ce1cdea25d880b4f2dcd3ba4
Tutemeana <148175219+Tutemeana@users.noreply.github.com>
2024-10-14 21:01:39 -0300

    Agregamos la clase Libro

--- src/Entity/Libro.php
+++ src/Entity/Libro.php
@@ -2,4 +2,55 @@
 
 Class Libro {
     
+    public $ID_Libro;
+    public $titulo;
+    public $descripcion;
+    public $precio;
+    public $stock;
+
+    function __construct(INT $ID_Libro, string $titulo, 
+        string $descripcion, int $precio, int $stock = 0)
+    {
+        $this->ID_Libro = $ID_Libro;
+        $this->titulo = $titulo;
+        $this->descripcion = $descripcion;
+        $this->precio = $precio;
+        $this->stock = $stock;
+    }
+
+    function getID(){
+        return $this->ID_Libro;
+    }
+    
+    function getTitulo(){
+        return $this->titulo;
+    }
+
+    function getDescripcion(){
+        return $this->descripcion;
+    }
+
+    function getPrecio(){
+        return $this->precio;
+    }
+
+    function getStock(){
+        return $this->stock;
+    }
+
+    function setTitulo($value): void{
+        $this->titulo = $value;
+    }
+
+    function setDescripcion($value): void{
+        $this->descripcion = $value;
+    }
+
+    function setPrecio($value): void{
+        $this->precio = $value;
+    }
+
+    function setStock($value): void{
+        $this->stock = $value;
+    }
 }
