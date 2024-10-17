dd36c87e9a23b1bc7a863b6ccb390cb1539dc231
Tutemeana <matute24b@gmail.com>
2024-10-16 22:51:49 -0300

    Corregi el composer

--- bootstrap.php
+++ bootstrap.php
@@ -1,22 +1,27 @@
 <?php
-
+// bootstrap.php
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
 
 require_once "vendor/autoload.php";
 
-// Crear una configuración simple de Doctrine ORM para atributos
+// Create a simple "default" Doctrine ORM configuration for Attributes
 $config = ORMSetup::createAttributeMetadataConfiguration(
-    paths: [__DIR__ . '/src/Entity'], // Asegúrate de que la ruta sea correcta
+    paths: [__DIR__ . '/src'],
     isDevMode: true,
 );
+// or if you prefer XML
+// $config = ORMSetup::createXMLMetadataConfiguration(
+//    paths: [__DIR__ . '/config/xml'],
+//    isDevMode: true,
+//);
 
-// Configurando la conexión a la base de datos
+// configuring the database connection
 $connection = DriverManager::getConnection([
     'driver' => 'pdo_sqlite',
-    'path' => __DIR__ . '/users.sqlite', 
+    'path' => __DIR__ . '/db.sqlite',
 ], $config);
 
-// Obteniendo el EntityManager
+// obtaining the entity manager
 $entityManager = new EntityManager($connection, $config);
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3312d7fc75b299f4c9d45e650748b57220e0098c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3312d7fc75b299f4c9d45e650748b57220e0098c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
